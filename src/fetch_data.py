d7406a46f1953b8523a5cd3da68586dc4b6e0bd7
worldwidesuperstar <elkilaranas1010@gmail.com>
2025-08-12 03:02:59 -0400

    feat: fixed shot data and did additional exploration

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -13,7 +13,7 @@ def fetch_top_200_pp48(season='2024-25'):
     df = leaders.get_data_frames()[0]
     top200 = df.head(200)
 
-    top200.to_csv('data/raw/top200_per.csv', index=False)
+    top200.to_csv('../data/raw/top200_per.csv', index=False)
     print('saved top 200 players by PTS/48 to CSV.')
     return top200
 
@@ -23,12 +23,12 @@ def fetch_save_advanced_data(df, season='2024-25', delay=5):
         player_id = row['PLAYER_ID']
         player_name = row['PLAYER']
 
-        general_data=retry_fetch_dashboard(player_id, season)
-        general_data.to_csv(f'data/raw/{player_id}_general_splits.csv', index=False)
-        print("saved general splits for ",player_name, ".")
+        # general_data=retry_fetch_dashboard(player_id, season)
+        # general_data.to_csv(f'data/raw/{player_id}_general_splits.csv', index=False)
+        # print("saved general splits for ",player_name, ".")
 
-        shot_data=retry_fetch_dashboard(player_id, season)
-        shot_data.to_csv(f'data/raw/{player_id}_shot_data.csv', index=False)
+        shot_data=retry_fetch_shotchart(player_id, season)
+        shot_data.to_csv(f'../data/raw/{player_id}_shot_data.csv', index=False)
         print("saved shot data for ",player_name, ".") 
 
         time.sleep(delay)
