9c99d0c242b141f26f59eef926e5bba148909988
worldwidesuperstar <elkilaranas1010@gmail.com>
2025-08-13 16:58:54 -0400

    feat: fixed shot data hopefully for the final time

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -13,7 +13,7 @@ def fetch_top_200_pp48(season='2024-25'):
     df = leaders.get_data_frames()[0]
     top200 = df.head(200)
 
-    top200.to_csv('../data/raw/top200_per.csv', index=False)
+    top200.to_csv('data/raw/top200_per.csv', index=False)
     print('saved top 200 players by PTS/48 to CSV.')
     return top200
 
@@ -28,7 +28,7 @@ def fetch_save_advanced_data(df, season='2024-25', delay=5):
         # print("saved general splits for ",player_name, ".")
 
         shot_data=retry_fetch_shotchart(player_id, season)
-        shot_data.to_csv(f'../data/raw/{player_id}_shot_data.csv', index=False)
+        shot_data.to_csv(f'data/raw/{player_id}_shot_data.csv', index=False)
         print("saved shot data for ",player_name, ".") 
 
         time.sleep(delay)
@@ -59,7 +59,7 @@ def retry_fetch_shotchart(player_id, season, max_retries=5, pause=5):
                 season_type_all_star='Regular Season',
                 season_nullable=season
             )
-            df = shots.get_data_frames()[1]
+            df = shots.get_data_frames()[0]
             return df
         except Exception as e:
             wait = pause * (attempt + 1)
